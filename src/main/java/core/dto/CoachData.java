84b8fcb1882ac11c0c53772a5377ff96669bce1f
ganjahnavarro <ganjahnavarro@gmail.com>
2018-08-31 09:26:01 +0800

    added width, height and commission fields

--- src/main/java/core/dto/CoachData.java
+++ src/main/java/core/dto/CoachData.java
@@ -1,5 +1,17 @@
 package core.dto;
 
+import java.math.BigDecimal;
+
 public class CoachData extends PersonData {
+
+	private BigDecimal commissionPercent;
+
+	public BigDecimal getCommissionPercent() {
+		return commissionPercent;
+	}
+
+	public void setCommissionPercent(BigDecimal commissionPercent) {
+		this.commissionPercent = commissionPercent;
+	}
 
 }
--- src/main/java/core/dto/MemberData.java
+++ src/main/java/core/dto/MemberData.java
@@ -1,10 +1,15 @@
 package core.dto;
 
+import java.math.BigDecimal;
+
 import core.enums.MemberType;
 
 public class MemberData extends PersonData {
 
 	private MemberType type;
+
+	private BigDecimal width;
+	private BigDecimal height;
 
 	public MemberType getType() {
 		return type;
@@ -12,6 +17,22 @@ public class MemberData extends PersonData {
 
 	public void setType(MemberType type) {
 		this.type = type;
+	}
+
+	public BigDecimal getWidth() {
+		return width;
+	}
+
+	public void setWidth(BigDecimal width) {
+		this.width = width;
+	}
+
+	public BigDecimal getHeight() {
+		return height;
+	}
+
+	public void setHeight(BigDecimal height) {
+		this.height = height;
 	}
 
 }
