91dd7de3f4543778f5cbb51b4e7d29a689ace1e6
ganjahnavarro <ganjahnavarro@gmail.com>
2018-09-07 09:40:38 +0800

    added updating of package availment remaining sessions

--- src/main/java/core/controller/TimeEntryController.java
+++ src/main/java/core/controller/TimeEntryController.java
@@ -1,7 +1,5 @@
 package core.controller;
 
-import java.math.BigDecimal;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import core.Utility;
 import core.dto.TimeEntryData;
 import core.mapper.TimeEntryMapper;
-import core.model.member.Member;
-import core.model.pkg.PackageAvailment;
-import core.model.program.ProgramAvailment;
 import core.model.timeentry.TimeEntry;
-import core.service.MemberService;
-import core.service.PackageAvailmentService;
-import core.service.ProgramAvailmentService;
 import core.service.TimeEntryService;
 
 @CrossOrigin
@@ -32,69 +23,25 @@ public class TimeEntryController {
 
 	@Autowired private TimeEntryService timeEntryService;
 
-	@Autowired private MemberService memberService;
-	@Autowired private ProgramAvailmentService programAvailmentService;
-	@Autowired private PackageAvailmentService packageAvailmentService;
-
 	private TimeEntryMapper MAPPER = TimeEntryMapper.INSTANCE;
 
 	@RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
-	public List<TimeEntryData> list(@RequestParam(value = "pageSize", required = false) Integer pageSize,
+	public List<TimeEntryData> list(
+			@RequestParam(value = "memberId", required = false) Long memberId,
+			@RequestParam(value = "coachId", required = false) Long coachId,
+			@RequestParam(value = "pageSize", required = false) Integer pageSize,
 			@RequestParam(value = "pageOffset", required = false) Integer pageOffset,
 			@RequestParam(value = "orderedBy", required = false) String orderedBy) {
-		return MAPPER.toData(timeEntryService.findFilteredItems(pageSize, pageOffset, orderedBy));
+		return MAPPER.toData(timeEntryService.findFilteredItems(memberId, coachId,
+				pageSize, pageOffset, orderedBy));
 	}
 
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public TimeEntryData create(@RequestBody TimeEntryData timeEntryData) {
 		TimeEntry timeEntry = MAPPER.fromData(timeEntryData);
-
-		timeEntry.setDate(new Date());
-
-		Member member = memberService.findByAccessCardNo(timeEntryData.getAccessCardNoUsed());
-		timeEntry.setMember(member);
-		timeEntry.setCoachAssigned(member.getDefaultCoach());
-
-		Availments availments = getAvailmentsToUse(member);
-		ProgramAvailment programAvailment = availments.programAvailment;
-		PackageAvailment packageAvailment = availments.packageAvailment;
-
-		timeEntry.setProgramAvailment(programAvailment);
-		timeEntry.setPackageAvailment(packageAvailment);
-		
-		BigDecimal commission = programAvailment != null ? programAvailment.getAvailedProgram().getCommission()
-				: packageAvailment.getAvailedPackage().getCommission();
-		timeEntry.setCommission(commission);
-
 		return MAPPER.toData((TimeEntry) timeEntryService.save(timeEntry));
 	}
 	
-	private Availments getAvailmentsToUse(Member member) {
-		Availments availments = new Availments();
-		Date today = Utility.getCurrentDateWithoutTime();
-
-		List<ProgramAvailment> availableProgramAvailments = programAvailmentService
-				.findAvailableProgramAvailments(member.getId(), today);
-		List<PackageAvailment> availablePackageAvailments = packageAvailmentService
-				.findAvailablePackageAvailments(member.getId(), today);
-		
-		ProgramAvailment programAvCandidate = availableProgramAvailments.get(0);
-		PackageAvailment packageAvCandidate = availablePackageAvailments.get(0);
-
-		if (programAvCandidate == null) {
-			availments.packageAvailment = packageAvCandidate;
-		} else if (packageAvCandidate == null) {
-			availments.programAvailment = programAvCandidate;
-		} else {
-			boolean packageBeforeProgram = packageAvCandidate.getStartDate()
-					.before(programAvCandidate.getStartDate());
-			availments.programAvailment = !packageBeforeProgram ? programAvCandidate : null;
-			availments.packageAvailment = packageBeforeProgram ? packageAvCandidate : null;
-		}
-
-		return availments;
-	}
-
 	@RequestMapping(value = "/", method = RequestMethod.PATCH)
 	public TimeEntryData update(@RequestBody TimeEntryData timeEntryData) {
 		TimeEntry timeEntry = MAPPER.fromData(timeEntryData);
@@ -104,13 +51,6 @@ public class TimeEntryController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public void delete(@PathVariable Long id) {
 		timeEntryService.deleteRecordById(id);
-	}
-
-	private class Availments {
-
-		ProgramAvailment programAvailment;
-		PackageAvailment packageAvailment;
-
 	}
 
 }
--- src/main/java/core/service/TimeEntryService.java
+++ src/main/java/core/service/TimeEntryService.java
@@ -1,5 +1,6 @@
 package core.service;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -7,6 +8,12 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import core.Utility;
+import core.model.IRecord;
+import core.model.member.Member;
+import core.model.pkg.PackageAvailment;
+import core.model.pkg.PackageAvailmentSession;
+import core.model.program.ProgramAvailment;
 import core.model.timeentry.TimeEntry;
 import core.model.timeentry.TimeEntryRepository;
 import core.repository.AbstractRepository;
@@ -16,6 +23,10 @@ import core.repository.AbstractRepository;
 public class TimeEntryService extends AbstractService {
 	
 	@Autowired private TimeEntryRepository repository;
+	
+	@Autowired private MemberService memberService;
+	@Autowired private ProgramAvailmentService programAvailmentService;
+	@Autowired private PackageAvailmentService packageAvailmentService;
 
 	@SuppressWarnings("rawtypes")
 	@Override
@@ -23,8 +34,91 @@ public class TimeEntryService extends AbstractService {
 		return repository;
 	}
 	
-	public List<TimeEntry> findFilteredItems(Integer pageSize, Integer pageOffset, String orderBy) {
-		return repository.findFilteredItems(pageSize, pageOffset, orderBy);
+	public List<TimeEntry> findFilteredItems(Long memberId, Long coachId,
+			Integer pageSize, Integer pageOffset, String orderBy) {
+		return repository.findFilteredItems(memberId, coachId,
+				pageSize, pageOffset, orderBy);
+	}
+	
+	@Override
+	public IRecord save(IRecord record) {
+		TimeEntry timeEntry = (TimeEntry) record;
+		
+		Member member = memberService.findByAccessCardNo(timeEntry.getAccessCardNoUsed());
+		if (member == null) {
+			throw new IllegalArgumentException("Member not found.");
+		}
+		timeEntry.setMember(member);
+		timeEntry.setCoachAssigned(member.getDefaultCoach());
+		
+		Date today = Utility.getCurrentDateWithoutTime();
+		
+		if (repository.findTimeEntryForDate(member.getId(), today) == null) {
+			timeEntry.setDate(today);
+
+			Availments availments = getAvailmentsToUse(member);
+			ProgramAvailment programAvailment = availments.programAvailment;
+			PackageAvailment packageAvailment = availments.packageAvailment;
+
+			if (programAvailment == null && packageAvailment == null) {
+				throw new IllegalArgumentException("No active program/package availed for member.");
+			}
+			
+			timeEntry.setProgramAvailment(programAvailment);
+			timeEntry.setPackageAvailment(packageAvailment);
+			
+			if (programAvailment != null) {
+				timeEntry.setCommission(programAvailment.getAvailedProgram().getCommission());
+			} else {
+				timeEntry.setCommission(packageAvailment.getAvailedPackage().getCommission());
+				
+				PackageAvailmentSession session = new PackageAvailmentSession();
+				session.setDate(timeEntry.getDate());
+				session.setPackageAvailment(timeEntry.getPackageAvailment());
+
+				packageAvailment.getSessions().add(session);
+				packageAvailment.setSessionsRemaining(packageAvailment.getSessionsRemaining() - 1);
+				super.update(packageAvailment);
+			}
+
+			return super.save(record);
+		}
+		return record;
+	}
+	
+	private Availments getAvailmentsToUse(Member member) {
+		Availments availments = new Availments();
+		Date today = Utility.getCurrentDateWithoutTime();
+
+		List<ProgramAvailment> availableProgramAvailments = programAvailmentService
+				.findAvailableProgramAvailments(member.getId(), today);
+		List<PackageAvailment> availablePackageAvailments = packageAvailmentService
+				.findAvailablePackageAvailments(member.getId(), today);
+		
+		ProgramAvailment programAvCandidate = !availableProgramAvailments.isEmpty() ?
+				availableProgramAvailments.get(0) : null;
+		PackageAvailment packageAvCandidate = !availablePackageAvailments.isEmpty() ?
+				availablePackageAvailments.get(0) : null;
+
+		if (programAvCandidate == null) {
+			availments.packageAvailment = packageAvCandidate;
+		} else if (packageAvCandidate == null) {
+			availments.programAvailment = programAvCandidate;
+		} else {
+			boolean packageBeforeProgram = packageAvCandidate.getStartDate()
+					.before(programAvCandidate.getStartDate());
+			availments.programAvailment = !packageBeforeProgram ? programAvCandidate : null;
+			availments.packageAvailment = packageBeforeProgram ? packageAvCandidate : null;
+		}
+
+		return availments;
+	}
+	
+	private class Availments {
+
+		ProgramAvailment programAvailment;
+		PackageAvailment packageAvailment;
+
 	}
 
 }
